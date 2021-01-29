13bfd7c11304947f12fb99bbfe6c2165e1be5cad
Drew Finnis <drewfinnis@gmail.com>
2021-01-29 19:43:00 +0100

    feature/level00_source.c

--- level00/source.c
+++ level00/source.c
@@ -1,1 +1,21 @@
-I mean... you know?
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	int	password;
+
+	puts("***********************************");
+	puts("* \t	 -Level00 -\t\t  *");
+	puts("***********************************");
+	printf("Password:");
+	scanf("%d", &password);
+	if (password == 0x149c)
+	{
+		puts("\nAuthenticated!");
+		system("/bin/sh");
+		return (0);
+	}
+	puts("\nInvalid Password!");
+	return (1);
+}
