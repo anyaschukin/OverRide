230f2415e374c4d8dfe6f23948b1fb3f1a047455
Anya Schukin <anya.schukin@gmail.com>
2021-02-03 15:29:42 +0100

    feature/add recreated binary

--- level03/source.c
+++ level03/source.c
@@ -1,1 +1,56 @@
-I mean... you know?
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+void decrypt(key)
+{
+	char *crypt = "Q}|u`sfg~sf{}|a3";
+	size_t len;
+	size_t i;
+	
+	len = strlen(crypt);
+	i = 0;
+	
+	while (i < len) {
+		crypt[i] = crypt[i] ^ key;
+		i++;
+	}
+	
+	if (strncmp(crypt, "Congratulations", 17) == 0) {
+    	system("/bin/sh");
+	}
+	else {
+		puts("\nInvalid Password");
+	}
+
+	return ;
+}
+
+void test(int input, int number)
+{
+	int key;
+	
+	key = number - input;
+	if (key > 21) {
+		decrypt(rand());
+	}
+	decrypt(key);
+	return ;
+}
+
+int main(void)
+{
+  int     input;
+
+  srand(time(0));
+
+  puts("***********************************");
+  puts("*\t\tlevel03\t\t**");
+  puts("***********************************");
+  printf("Password:");
+
+  scanf("%d", &input);
+  test(input, 322424845);
+  return (0);
+}
