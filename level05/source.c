ac2fdebd77c4d897f6640fd034d18d9aa247eff4
Anya Schukin <anya.schukin@gmail.com>
2021-02-06 00:42:38 +0100

    feature/add recreated binary

--- level05/source.c
+++ level05/source.c
@@ -1,1 +1,20 @@
-I mean... you know?
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    char buffer[100];
+    int i;
+    
+    fgets(buffer, 100, stdin);
+    i = 0;
+    while (i < strlen(buffer))
+	{
+        if (buffer[i] > 0x40 && buffer[i] <= 0x5a);
+            buffer[i] = buffer[i] ^ 0x20; // tolower()
+        i++;
+    }
+    printf(buffer);
+    exit(0);
+}
